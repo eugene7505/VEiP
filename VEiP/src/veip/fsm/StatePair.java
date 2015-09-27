e5668b4ae78668bd50be228f29087fefd7a96216
Yi-Chin <yi-chinwu@192.168.1.7>
2015-09-26 23:09:53 -0700

    Adding StatePair class

---
+++ VEiP/src/veip/fsm/StatePair.java
@@ -0,0 +1,13 @@
+package veip.fsm;
+
+
+public class StatePair {
+	public State first;
+	public State second;
+
+	public StatePair(State first, State second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+}
